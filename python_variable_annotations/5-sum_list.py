f837c01401279e1fadf04b5aea84a12c4f2cc933
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-03 16:36:37 -0400

    Update sum_list function to check input list for floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,6 +3,9 @@ from typing import List
 """Module that calculates the sum of a list of floats"""""
 
 
-def sum_list(input_list: List[float]) -> float:
+def sum_list(input_list: list) -> float:
     """Calculate the sum of a list of floats"""
-    return sum(input_list)
+    if all(isinstance(item, float) for item in input_list):
+        return sum(input_list)
+    else:
+        raise TypeError("Input list should contain only floats")
