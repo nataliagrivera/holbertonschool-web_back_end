e7f6d6a32ca7b545beb09d943f0bc22c6de6a6fd
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-02 23:44:05 -0400

    Fix formatting in add function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Basic annotatios - add"""
-def add(a:float,b:float) -> float:
+
+
+def add(a: float, b: float) -> float:
     """add two floats and return a float"""
     return a + b
