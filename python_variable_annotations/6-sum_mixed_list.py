86dd4f9c9cc32219c129a8667a7b7441b2f0adc2
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-03 17:24:24 -0400

    Fix variable annotations in sum_mixed_list function

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from typing import Union, List
 """Basic annotations - mixed list"""
 
 
-def sum_mixed_list(mxd_lst: List[Union(int, float)]) -> float:
-    """Calculate the sum of a list of floats and integers"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Calculate the sum of a list of floats and ints"""
     return sum(mxd_lst)
