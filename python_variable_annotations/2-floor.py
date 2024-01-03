ae7b953a06b1d3b616f142cb7ca6e7e0739108df
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-03 16:10:27 -0400

    Refactor floor function to use int casting instead of math.floor

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-import math
 """Basic annotations - floor"""
 
 
 def floor(n: float) -> int:
     """floor a float and return an int"""
-    return math.floor(n)
+    return int(n)
