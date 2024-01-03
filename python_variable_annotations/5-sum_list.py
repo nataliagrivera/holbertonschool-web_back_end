76ea53786ed95c23e8791d9c39b50038dc2e2be7
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-03 17:25:20 -0400

    Add type hinting for input_list parameter in sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
 """Module that calculates the sum of a list of floats"""""
+
+
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
