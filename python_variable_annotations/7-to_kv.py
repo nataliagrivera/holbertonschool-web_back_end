aaeb71e3f5d84fe9529525353babe80ed66c6ba0
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-03 21:44:29 -0400

    Fix type annotation in to_kv function

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,6 +5,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k:str, v: Tuple[Union[int, float]]) -> Tuple[str,float]:
+def to_kv(k: str, v: Tuple[Union[int, float]]) -> Tuple[str, float]:
     """Returns a tuple"""
     return (k, v**2)
