10f1c15ff444c8c4e9def9db6597ba420ad359a7
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-05 18:58:46 -0400

    Fix import formatting in basic async syntax

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 """Basic async syntax"""
 
 
 import random
-import asyncio 
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
