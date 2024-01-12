c31d1d9d45363c2b2ded6466a26020e4571a2f41
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-12 00:50:41 -0400

    Add log stats script to provide statistics about nginx logs stored in MongoDB

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Log stats"""
+
+from pymongo import MongoClient
+
+
+def helper(a: dict) -> int:
+    """Helper function"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    col = client.logs.nginx
+    return col.count_documents(a)
+
+
+def main():
+    """Provides stats about nginx logs stored in MongoDB"""
+    print(f"{helper({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {helper({'method': 'GET'})}")
+    print(f"\tmethod POST: {helper({'method': 'POST'})}")
+    print(f"\tmethod PUT: {helper({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {helper({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {helper({'method': 'DELETE'})}")
+    print(f"{helper({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
