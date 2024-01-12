5baabe4b3a60d4d0197f7595ae950c3edb340d50
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-12 19:23:26 -0400

    Add docstrings to functions

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,21 +5,25 @@
 from pymongo import MongoClient
 
 def get_logs_count():
+    """Get logs count"""
     with MongoClient('mongodb://127.0.0.1:27017') as client:
         col = client.logs.nginx
         return col.count_documents({})
 
 def get_method_count(method):
+    """Get method count"""
     with MongoClient('mongodb://127.0.0.1:27017') as client:
         col = client.logs.nginx
         return col.count_documents({'method': method})
 
 def get_status_check_count():
+    """Get status check count"""
     with MongoClient('mongodb://127.0.0.1:27017') as client:
         col = client.logs.nginx
         return col.count_documents({'method': 'GET', 'path': '/status'})
 
 def main():
+    """Main function"""
     logs_count = get_logs_count()
     print(f"{logs_count} logs")
 
