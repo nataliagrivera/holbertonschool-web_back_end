289bca85c58f49b469b576b369f04b95bbf0c46b
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-17 18:32:20 -0400

    Refactor getBudgetForCurrentYear function

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,10 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-
-  return { 
-[`income-${getCurrentYear()}`]: income,
-[`gdp-${getCurrentYear()}`]: gdp,
-[`capita-${getCurrentYear()}`]: capita,
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
 
   };
 }
