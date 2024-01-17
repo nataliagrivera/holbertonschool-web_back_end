051a9f16afb5f769759964b69639fda024ae9368
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-17 16:28:15 -0400

    Fix assignment bug in getBudgetObject function

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -6,7 +6,7 @@ export default function getBudgetObject(income, gdp, capita) {
   };
 
   Object.keys(budget).forEach(key => {
-    budget[key] = `${budget[key]}` ;
+    budget[key] = budget[key] ;
   });
 
   return budget;
