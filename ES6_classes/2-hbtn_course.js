e8182a5b627ff29cb8b9ea265deeacefbb5e6cf5
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-19 13:40:42 -0400

    Refactor HolbertonCourse class with proper data type validations and encapsulation

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,9 +1,41 @@
-export default class HolbertonCourse() {
-    
-    constructor(name, length, students) 
-    {
-        this.name = name;
-        this.length = length;
-        this.students = students;
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        if (typeof name != 'string') {
+            throw TypeError('Name muust be a string')
+        }
+        if (typeof length != 'number'){
+            throw TypeError('Lenght must be a number')
+        }
+        if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+            throw TypeError('Students must be an array of strings')
+        }
+
+        this._name = name;
+        this._length = length;
+        this._students = students;
+    }
+
+    get name() {
+        return this._name
+    }
+
+    set name(newName) {
+        this._name = newName
+    }
+        
+    get length() {  
+        return this._length
+    }
+
+    set length(newLength) {
+        this._length = newLength
+    }
+
+    get students() {
+        return this._students
+    }
+
+    set students(newStudents) {
+        this._students = newStudents
     }
 }
