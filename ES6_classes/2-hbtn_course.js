25450ce1c5e42ad19bbde0ada284fad7ca140b6e
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-19 10:17:03 -0400

    Add HolbertonCourse class definition

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,9 @@
+export default class HolbertonCourse() {
+    
+    constructor(name, length, students) 
+    {
+        this.name = name;
+        this.length = length;
+        this.students = students;
+    }
+}
