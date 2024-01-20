c8665407fd266d85d35401b748f8332ada6a456d
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-19 20:12:21 -0400

    Refactor Currency class constructor and setters
    This commit refactors the Currency class constructor and setters to improve code readability and maintainability. The changes include proper indentation, adding semicolons, and throwing type errors for invalid inputs.

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,39 +1,38 @@
 export default class Currency {
-  constructor(code, name) {
-      if (typeof code !== 'string') {
-        throw TypeError('Code must be a string')
-      }
-      if (typeof name !== 'string') {
-        throw TypeError('Name must be a string')
-      }
-      this._code = code;
-      this._name = name;
+    constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string')
+    }
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
-      return this._code;
+    return this._code;
   }
 
   set code(newCode) {
         if (typeof newCode !== 'string') {
-            throw TypeError('Code must be a string')
-        }
-        this._code = newCode;
+      throw TypeError('Code must be a string');
+      }
+    this._code = newCode;
   }
 
   get name() {
-        return this._name;
+    return this._name;
   }
 
   set name(newName) {
-        if (typeof newName !== 'string') {
-            throw TypeError('Name must be a string')
-        }
-        this._name = newName;
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+      }
+    this._name = newName;
   }
 
   displayFullCurrency() {
-    return `${this.name} (${this.code})`
+    return `${this.name} (${this.code})`;
   }
-
 }
