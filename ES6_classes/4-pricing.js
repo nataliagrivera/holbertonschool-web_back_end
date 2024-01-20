3cb05c2095656f78dfae66918167621ae5e2a20d
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-19 20:31:31 -0400

    Add Pricing class with getters, setters, and methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,50 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor (amount, currency) {
+    if (typeof amount !== 'number') {
+      throw TypeError('amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw TypeError('currency must be a Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw TypeError('amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw TypeError('currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
