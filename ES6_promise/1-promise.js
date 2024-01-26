f3bc3ea9493574bca402891f8970e3a6b24c7de0
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-26 12:04:19 -0400

    Add getFullResponseFromAPI function to handle API responses

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(`status: 200, body: Success`);
+    } else {
+      reject(`Error: 404, body: Not found`);
+    }
+  });
+}
