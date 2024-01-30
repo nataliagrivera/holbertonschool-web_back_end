b5f0e7dfaf8de710688335708558f8de02150ce3
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-30 17:04:04 -0400

    Refactor getStudentsByLocation to accept a targetLocation parameter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,5 @@
 // 0-get_students.js
 
-export default function getStudentsByLocation(students) {
-  return students.filter(student => student.location === 'San Francisco');
+export default function getStudentsByLocation(students, targetLocation) {
+  return students.filter(student => student.location === targetLocation);
 }
