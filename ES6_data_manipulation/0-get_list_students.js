f6ddfef53a1f8dc72cac0f6b95281e447ad0634e
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-30 12:06:11 -0400

    Fix missing comma in student object

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,9 +2,9 @@ function getListStudents() {
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
-  
+
   return students;
 }
 
