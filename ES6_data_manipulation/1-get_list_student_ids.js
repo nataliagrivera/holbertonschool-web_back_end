d50faf878b89443b7408407f873e8a57f76200f9
nataliagrivera <nataliagirvera62@gmail.com>
2024-01-30 16:27:53 -0400

    Refactor getListStudentIds function to use ES6 syntax

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-function getListStudentIds(students) {
-  if (students === null) {
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
     return [];
   }
-  return students.map((student) => student.id);
+  return students.map(student => student.id);
 }
