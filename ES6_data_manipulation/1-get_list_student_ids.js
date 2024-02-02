b996bd2cd62b407196b9bd84e8737675664402f8
nataliagrivera <nataliagirvera62@gmail.com>
2024-02-01 20:51:56 -0400

    Refactor arrow function syntax in getListStudentIds

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(array) {
   if (!Array.isArray(array)) {
     return [];
   }
-  return array.map(student => student.id);
+  return array.map((student) => student.id);
 }
